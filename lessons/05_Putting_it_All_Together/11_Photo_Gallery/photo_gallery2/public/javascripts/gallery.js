cc65bc797aa2bfbda4e3a70be61056e970473f1b
Daniel Chae <daniel.jiwoong@gmail.com>
2025-01-23 18:25:23 +0700

    oop gallery refactor

--- lessons/05_Putting_it_All_Together/11_Photo_Gallery/photo_gallery2/public/javascripts/gallery.js
+++ lessons/05_Putting_it_All_Together/11_Photo_Gallery/photo_gallery2/public/javascripts/gallery.js
@@ -1,87 +1,123 @@
 "use strict";
 /* eslint max-lines-per-function: "off" */
 
-let curIndex = 0;
-
-// function getCurrentPhotoId() {
-//   const currentlyDisplayed = document.querySelector(
-//     "#slides figure:not([display='none'])",
-//   );
-//   return +currentlyDisplayed.dataset.id;
-// }
-
-function renderSlides(templates, photos) {
-  const html = templates["photos"]({ photos });
-  const slides = document.querySelector("#slides");
-  slides.innerHTML = html;
+function inInput() {
+  const active = document.activeElement;
+  return active && ["INPUT", "TEXTAREA"].includes(active.tagName);
 }
 
-function renderPhotoInfo(templates, photo) {
-  const html = templates["photo_information"](photo);
-  document.querySelector("section header").innerHTML = html;
+class Slideshow {
+  constructor(photos) {
+    this.templates = {};
+    this.initHandlebars();
+    this.curIndex = 0;
+    this.photos = photos;
+    this.slidesContainer = document.querySelector("#slides");
+    this.renderSlides();
+    this.slides = [...document.querySelectorAll(`#slides figure`)];
+    this.initEventHandlers();
+  }
+
+  // photo() and slide() synchronisation depends on Handlebars template #each
+  get photo() {
+    return this.photos[this.curIndex];
+  }
+
+  get slide() {
+    return this.slides[this.curIndex];
+  }
+
+  next(step = 1) {
+    this.curIndex = this.curIndex + step + this.photos.length;
+    this.curIndex %= this.photos.length;
+    return this.photo;
+  }
+
+  renderSlides() {
+    const photos = this.photos;
+    const renderedHtml = this.templates["photos"]({ photos });
+    this.slidesContainer.innerHTML = renderedHtml;
+  }
+
+  renderCurrent() {
+    this.renderPhotoInfo(this.photo);
+    this.renderComments(this.photo);
+  }
+
+  renderPhotoInfo(photo) {
+    const renderedHtml = this.templates["photo_information"](photo);
+    document.querySelector("section header").innerHTML = renderedHtml;
+  }
+
+  async renderComments(photo) {
+    const response = await fetch(`comments?photo_id=${photo.id}`);
+    const comments = await response.json();
+
+    const renderedHtml = this.templates["photo_comments"]({ comments });
+    const commentsList = document.querySelector("#comments ul");
+    commentsList.innerHTML = renderedHtml;
+  }
+
+  nextHandler(e, step = 1) {
+    e.preventDefault();
+    // fade in next slide
+    this.slide.classList.add("hidden");
+    this.next(step);
+    this.slide.classList.remove("hidden");
+
+    // update photo info + comments
+    this.renderCurrent();
+  }
+
+  prevHandler(e, step = -1) {
+    this.nextHandler(e, step);
+  }
+
+  arrowKeyHandler(e) {
+    if (inInput()) return;
+
+    switch (e.key) {
+      case "ArrowLeft":
+        this.prevHandler(e);
+        break;
+      case "ArrowRight":
+        this.nextHandler(e);
+        break;
+    }
+  }
+
+  initHandlebars() {
+    // register all partials
+    document.querySelectorAll("[data-type='partial']").forEach((element) => {
+      Handlebars.registerPartial("comment", element.innerHTML);
+    });
+
+    // compile and remove all templates
+    document
+      .querySelectorAll("[type='text/x-handlebars']")
+      .forEach((element) => {
+        this.templates[element.id] = Handlebars.compile(element.innerHTML);
+        element.remove();
+      });
+  }
+
+  initEventHandlers() {
+    // add event listeners for previous and next anchors
+    const nextAnchor = document.querySelector("a.next");
+    nextAnchor.addEventListener("click", this.nextHandler.bind(this));
+
+    const prevAnchor = document.querySelector("a.prev");
+    prevAnchor.addEventListener("click", this.prevHandler.bind(this));
+
+    // add event listeners for left and right keys
+    document.addEventListener("keydown", this.arrowKeyHandler.bind(this));
+  }
 }
-
-async function renderComments(templates, photo) {
-  const response = await fetch(`comments?photo_id=${photo.id}`);
-  const comments = await response.json();
-
-  const html = templates["photo_comments"]({ comments });
-  const commentsList = document.querySelector("#comments ul");
-  commentsList.innerHTML = html;
-}
-
-function stepSlideHandler(event, { forward, templates, photos }) {
-  event.preventDefault();
-  // fade in next slide
-  const slides = document.querySelectorAll(`#slides figure`);
-  const step = forward ? 1 : -1;
-  const nextIndex = (curIndex + step + slides.length) % slides.length;
-
-  slides[curIndex].classList.add("hidden");
-  slides[nextIndex].classList.remove("hidden");
-  curIndex = nextIndex;
-
-  // update photo info + comments
-  const photo = photos.find(({ id }) => id === +slides[curIndex].dataset.id);
-  renderPhotoInfo(templates, photo);
-  renderComments(templates, photo);
-}
-
-// refactor this trash
-// add a state object or OOP? remove global curIndex somehow
 
 document.addEventListener("DOMContentLoaded", async () => {
-  // compile all templates
-  let templates = {};
-  document.querySelectorAll("[type='text/x-handlebars']").forEach((element) => {
-    templates[element.id] = Handlebars.compile(element.innerHTML);
-  });
-
-  // register all partials
-  document.querySelectorAll("[data-type='partial']").forEach((element) => {
-    Handlebars.registerPartial("comment", element.innerHTML);
-  });
-
   // get photos json data
   const photos = await fetch("/photos").then((res) => res.json());
+  const slideshow = new Slideshow(photos);
 
-  // render all slides
-  renderSlides(templates, photos);
-
-  // render photo_information template
-  renderPhotoInfo(templates, photos[0]);
-  // render comments
-  renderComments(templates, photos[0]);
-
-  // add event listeners for previous and next anchors
-  document
-    .querySelector("a.next")
-    .addEventListener("click", (e) =>
-      stepSlideHandler(e, { forward: true, templates, photos }),
-    );
-  document
-    .querySelector("a.prev")
-    .addEventListener("click", (e) =>
-      stepSlideHandler(e, { forward: false, templates, photos }),
-    );
+  slideshow.renderCurrent();
 });
